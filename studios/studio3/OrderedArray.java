c8e62f35bd4e768f0c2a16c295cb1ca21d7f2e56
Gillian <Gillian@gillians-mbp.dhcp.wustl.edu>
2018-02-08 15:58:30 -0600

    studio 3

--- studios/studio3/OrderedArray.java
+++ studios/studio3/OrderedArray.java
@@ -13,25 +13,45 @@ public class OrderedArray<T extends Comparable<T>> implements PriorityQueue<T> {
 	
 	@Override
 	public boolean isEmpty() {
-		//
-		// FIXME
-		//
-		return false;
+		if (array.length == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	@Override
 	public void insert(T thing) {
-		//
-		// FIXME
-		//
+		if (size == 0) {
+			array[array.length-1] = thing;
+		}
+		else {
+			//int index = 0;
+			for (int i = size; i >= 0; --i) {
+				if (thing.compareTo(array[i]) > 0) { //thing is greater than array at i
+					//index = i;
+					for (int j = i; j >= 0; j--) {
+						array [j+1] = array [j];
+					}
+					array [i] = thing;
+					break;
+				}
+			}
+		}
+		size = size + 1;
 	}
 	
 	@Override
 	public T extractMin() {
-		//
-		// FIXME
-		//
-		return null;
+		T min = array [size];
+//		for (int i = 1; i < size-1; ++i) {
+//			array [i-1] = array [i];
+//		}
+		return min;
+		
+		
+		
 	}
 
 }
--- studios/studio3/UnorderedList.java
+++ studios/studio3/UnorderedList.java
@@ -6,32 +6,38 @@ import java.util.List;
 public class UnorderedList<T extends Comparable<T>> implements PriorityQueue<T> {
 
 	public List<T> list;
-	
+
 	public UnorderedList() {
 		list = new LinkedList<T>();
 	}
-	
+
 	@Override
 	public boolean isEmpty() {
-		//
-		// FIXME
-		//
-		return false;
+		if (list.size() == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	@Override
 	public void insert(T thing) {
-		//
-		// FIXME
-		//
+		list.add(thing);
 	}
 
 	@Override
 	public T extractMin() {
-		//
-		// FIXME
-		//
-		return null;
-	}
+		T min = list.get(0);
+		for (T t: list) {
+			if (t.compareTo(min) < 0 ) {
+				min = t;
+			}
+		}
+		list.remove(min);
+		return min;
 
+
+	}
 }
+

4a5b1cff52e40db587ec4fa2d5e17ed7e68f747b
Gillian <Gillian@gillians-mbp.dhcp.wustl.edu>
2018-04-02 18:36:26 -0500

    lab9

--- labs/rabinkarp/RK.java
+++ labs/rabinkarp/RK.java
@@ -8,13 +8,32 @@ public class RK {
 	//  of its requirements
 	//
 	
+	//do i need these instance variables?
+	private int m; 
+	private String query;
+	private String target;
+	private int h;
 
 	/**
 	 * Rabin-Karp string matching for a window of the specified size
 	 * @param m size of the window
 	 */
 	public RK(int m) {
-
+		this.m = m;
+//		this.query = query;
+//		this.target = target;
+//		this.h = h;
+		
+		for(int i = 0; i<target.length(); ++i) {
+			char[] charArray = query.toCharArray(); //gets the value of the rolling hash of the first value in the query string
+			int sum = 0;
+			for (int j = 0; j<m; ++j) {
+				sum = sum + nextCh(charArray[j]); //at the end of loop, sum will be the hash for query
+			}
+			
+		
+		}
+		
 	}
 	
 
@@ -23,8 +42,30 @@ public class RK {
 	 * @param d the next character in the target string
 	 * @return
 	 */
+	//must take theta(1) time
+	//incremental approach
 	public int nextCh(char d) {
-		return 0;
+		
+		int [] circBuffer = new int [m];
+		char [] targetChars = target.toCharArray();
+		int index = target.indexOf(d);
+		
+		if(index < m-1) {
+			return 0; //h = 0
+		}
+		
+		//load the first 4 chars of target into circular buffer
+		for (int i = (index - m + 1); i < index; i++) {
+			circBuffer[i] = targetChars[i];
+			h = (int) (31*h - Math.pow(31, m)*(index - m) + i) % 511; //change me!
+		}
+		
+				
+			
+		int rollingHash = 0;
+		return rollingHash;
+
+		
 	}
 
 }

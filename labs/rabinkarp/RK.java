c784f62115d586eac12b779cc974e79cbb2e054b
Gillian <Gillian@gillians-mbp.dhcp.wustl.edu>
2018-04-02 22:05:08 -0500

    lab 9

--- labs/rabinkarp/RK.java
+++ labs/rabinkarp/RK.java
@@ -1,18 +1,20 @@
 package rabinkarp;
 
 public class RK {
-	
+
 	//
 	// Be sure to look at the write up for this assignment
 	//  so that you get full credit by satisfying all
 	//  of its requirements
 	//
-	
+
 	//do i need these instance variables?
 	private int m; 
 	private String query;
 	private String target;
 	private int h;
+	private char circBuffer [];
+	private int counter;
 
 	/**
 	 * Rabin-Karp string matching for a window of the specified size
@@ -20,22 +22,13 @@ public class RK {
 	 */
 	public RK(int m) {
 		this.m = m;
-//		this.query = query;
-//		this.target = target;
-//		this.h = h;
-		
-		for(int i = 0; i<target.length(); ++i) {
-			char[] charArray = query.toCharArray(); //gets the value of the rolling hash of the first value in the query string
-			int sum = 0;
-			for (int j = 0; j<m; ++j) {
-				sum = sum + nextCh(charArray[j]); //at the end of loop, sum will be the hash for query
-			}
-			
-		
-		}
-		
+		h = 0;
+		circBuffer = new char [m];
+		counter = 0;
+
+
 	}
-	
+
 
 	/**
 	 * Compute the rolling hash for the previous m-1 characters with d appended.
@@ -46,26 +39,28 @@ public class RK {
 	//incremental approach
 	public int nextCh(char d) {
 		
-		int [] circBuffer = new int [m];
-		char [] targetChars = target.toCharArray();
-		int index = target.indexOf(d);
-		
-		if(index < m-1) {
-			return 0; //h = 0
-		}
-		
-		//load the first 4 chars of target into circular buffer
-		for (int i = (index - m + 1); i < index; i++) {
-			circBuffer[i] = targetChars[i];
-			h = (int) (31*h - Math.pow(31, m)*(index - m) + i) % 511; //change me!
-		}
-		
-				
-			
-		int rollingHash = 0;
-		return rollingHash;
+		int a = circBuffer[counter];
+		circBuffer[counter] = d;
 
-		
+		h =  Math.floorMod((31*h - power(m)*a + d), 511); 
+
+		counter = (counter+1) % m;
+		return h;
+
 	}
+
+	public int power (int m) {
+		int sum = 1;
+		if(m == 0) {
+			return 0;
+		}
+		for (int i = 0; i < m; i++) {
+
+			sum = (sum * 31)%511;
+		}
+		return sum;
+	}
+
+
 
 }
